4143cdbad1eabe2ddcc24da03fb05a0a7a281fd2
David <dmuriu0@gmail.com>
2025-07-12 13:46:18 +0300

    Events Listener

--- index.js
+++ index.js
@@ -1,2 +1,23 @@
 function addingEventListener() {
+  const button = document.getElementById('button');
+  button.addEventListener('click', function() {
+    require ( './helpers.js' );
+    
+    const sinon = require('sinon');
+    
+    describe("index.js", () => {
+      let input;
+    
+      beforeEach(function() {
+        input = document.getElementById('button');
+        sinon.spy(input, 'addEventListener');
+      })
+    
+      it("binds an event listener in addingEventListener()", () => {
+        addingEventListener();
+        expect(input.addEventListener.called).to.be.true;
+      })
+    })
+    
+  });
 }
